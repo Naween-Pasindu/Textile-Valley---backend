5cc9b0db44f1e3f013753e194ca10495b5e852ea
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2022-09-04 12:22:29 +0530

    initializing spring boot

---
+++ src/test/java/com/example/TextileValley/TextileValleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TextileValley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextileValleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
