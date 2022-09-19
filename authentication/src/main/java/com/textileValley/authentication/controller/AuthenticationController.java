43d31bec2a213a6774d938bf0279d1b768da6fbc
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2022-09-19 18:20:42 +0530

    creating AuthenticationApplication

--- authentication/src/main/java/com/textileValley/authentication/controller/AuthenticationController.java
+++ authentication/src/main/java/com/textileValley/authentication/controller/AuthenticationController.java
@@ -1,8 +1,19 @@
 package com.textileValley.authentication.controller;
 
+import com.textileValley.authentication.entity.AuthenticationEntity;
+import com.textileValley.authentication.util.AuthenticationUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
 
+    @Autowired
+    private AuthenticationUtil authenticationUtil;
+    @PostMapping("/validate")
+    public String generateToken(@RequestBody AuthenticationEntity authRequest) throws Exception {
+        return "fgf";
+    }
 }
